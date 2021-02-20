1b153a32c0d3050acab25f0449c705e220360476
Beyza Karadeniz <byzakrdnzz@gmail.com>
2021-02-20 17:59:56 +0300

    main screen ui design

--- lib/ui/root_screen.dart
+++ lib/ui/root_screen.dart
@@ -6,8 +6,37 @@ class RootScreen extends StatefulWidget {
 }
 
 class _RootScreenState extends State<RootScreen> {
+  int defaultTabLength = 3;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: DefaultTabController(
+          length: defaultTabLength,
+          child: Column(
+            children: <Widget>[
+              _containerAppbar,
+              _tabBarItems,
+              Expanded(
+                child: Container(),
+              )
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: _fatButton,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
   }
+
+  Widget get _appBar => AppBar(title: Text('Nature Save'));
+
+  Widget get _fatButton => FloatingActionButton(
+      onPressed: () {}, child: Icon(Icons.search)); // !!! bbak
+  Widget get _containerAppbar => Container(height: 50, child: _appBar);
+  Widget get _tabBarItems => TabBar(tabs: <Widget>[
+        Tab(icon: Icon(Icons.map)),
+        Tab(icon: Icon(Icons.home)),
+        Tab(icon: Icon(Icons.person_pin))
+      ]);
 }
